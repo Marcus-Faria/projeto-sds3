61ffb118b12cbcd075fdddd4d79da7792d875ac8
Marcus-Faria <marcusviniciusfaria150@gmail.com>
2021-11-03 16:17:51 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Não sei o que esse cara está fazendo!</h1>
+      <h1 className="text-primary">Não sei o que esse cara está fazendo!</h1>
     </div>
   );
 }
