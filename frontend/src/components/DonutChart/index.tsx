f246183fe24f72b880a0bdef060727ad7fa87e7f
Marcus-Faria <marcusviniciusfaria150@gmail.com>
2021-11-07 17:59:12 -0300

    Pagination

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,7 +14,7 @@ function DonutChart() {
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
-        axios.get("${BASE_URL}/sales/amount-by-seller").then(response => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`).then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map((x) => x.sellerName);
             const mySeries = data.map((x) => x.sum);
